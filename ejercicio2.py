f7a4410da19d13fc57b6fb17e0e2c049c5f889e3
marianita222 <quinteromariana291@gmail.com>
2025-07-21 12:03:26 -0400

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,5 @@
+nota = int(input("¿cual es tu nota?"))
+if nota >= 17: 
+    print ("aprobado")
+else:
+    print ("desaprobado")
---
+++ ejerciciotemp.py
@@ -0,0 +1,6 @@
+temperatura = int(input("¿que temperatura hace hoy en ºc?"))
+if temperatura > 20: 
+    print ("una chaqueta ligera esta bien")
+else:
+    print ("ponte un abrigo hace frio")
+
