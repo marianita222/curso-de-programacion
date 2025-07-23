c7411211eccf70e44e383282d87054895038e59c
marianita222 <quinteromariana291@gmail.com>
2025-07-23 13:03:38 -0400

    Add files via upload

---
+++ ejemplo4.py
@@ -0,0 +1,17 @@
+
+
+
+# uso de funciones de python
+def formular():
+    print("area = base * altura" )
+
+    base = int(input("numero 10"))
+    altura = int(input("numero 15"))
+    
+    area = base * altura
+
+formular()
+               
+    
+
+    
