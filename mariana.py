ce520bb4b58f9a4e9ce178e3ad36c4640e151d5f
marianita222 <quinteromariana291@gmail.com>
2025-07-23 13:06:47 -0400

    Add files via upload

--- mariana.py
+++ mariana.py
@@ -1,26 +1,26 @@
-opcion = input("estas caminando por un bosque oscuro y encuentras dos objetos: un *FOSFORO* O una *LINTERNA* con cual te quedas?")
+opcion = input("estas caminando por un bosque oscuro y encuentras dos objetos: un *Fosforo* O una *linterna* con cual te quedas?").lower()
 print()
-if opcion == "LINTERNA":
-    print("la linterna esta encendida")
+if opcion == "linterna":
+  print("la linterna esta encendida")
 print()
-opcion = input("quieres *SEGUIR* el camino o *BUSCAR* entre los arboles lo que hizo el ruido?")
+opcion = input("quieres *seguir* el camino o *buscar* entre los arboles lo que hizo el ruido?")
 print()
-if opcion == "BUSCAR":
-    print("buscaste donde estaba ese misterioso ruido,pero escuchaste el mismo sonido detras de ti")
-    opcion = input("deseas *CORRER* o *VOLTEAR*")
+if opcion == "buscar":
+ print("buscaste donde estaba ese misterioso ruido,pero escuchaste el mismo sonido detras de ti")
+opcion = input("deseas *CORRER* o *VOLTEAR*")
 print()
-if opcion == "CORRER":
-    print("corriste lo mas rapido que pudiste, sin emabrgo, sentias que cada vez estabas mas cerca, mientras corrias te tropezaste con una piedra y te desmayaste")
-    opcion = input("despertaste mareado y lo unico que tienes a tu alrededor es una casa abandonada, deseas entrar? *SI* *NO*")
+if opcion.lower == "correr":
+     print("corriste lo mas rapido que pudiste, sin emabrgo, sentias que cada vez estabas mas cerca, mientras corrias te tropezaste con una piedra y te desmayaste")
+opcion = input("despertaste mareado y lo unico que tienes a tu alrededor es una casa abandonada, deseas entrar? *si* *no*")
 print()
-if opcion == "SI":
-    print("entraste a la casa abandonada, observaste que todo estaba hecho pedazos y muy tenebroso")
-    opcion = input("quieres pasar la noche en la casa abandonada? *PASAR LA NOCHE* *DEJAR LA CASA*")
-if opcion == "PASAR LA NOCHE":
+if opcion == "si":
+      print("entraste a la casa abandonada, observaste que todo estaba hecho pedazos y muy tenebroso")
+opcion = input("quieres pasar la noche en la casa abandonada? *pasar la noche* *dejar la casa*")
+if opcion == "pasar la noche":
     print("te quedaste dormido hasta que luego volviste a escuchar el mismo sonido que te perseguia")
-else:
     print("no debiste quedarte en la casa, quien habitaba alli era un oso tenebroso y muerto de hambre, terminaste devorado XD")
     print()
+
 
 if opcion == "FOSFORO":
     print("lograste encender un fosforo")
@@ -29,8 +29,8 @@ if opcion == "FOSFORO":
      print("solo te quedan 2 fosforos")
     elif opcion1 == "SI":
         print("esperaste demasiado, el frio de la lluvia y la oscuridad te consumieron a tal grado que moriste de hipotermia")
-else:
-    print("habia mucha lluvia y no pudiste prender ningun fosforo, un tenebroso oso grizly te devora por completo en la ocuridad.")
+    else:
+      print("habia mucha lluvia y no pudiste prender ningun fosforo, un tenebroso oso grizly te devora por completo en la oscuridad.")
     opcion = input("fin de juego,deseas volver a jugar? *VOLVER A JUGAR* *SALIR DEL JUEGO*")    
 
 
