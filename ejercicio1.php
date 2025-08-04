3b366c279eb5ec220239435a45a03294457e3d0d
marianita222 <quinteromariana291@gmail.com>
2025-08-04 10:44:20 -0400

    Add files via upload

---
+++ ejercicio1.php
@@ -0,0 +1,8 @@
+<?php
+
+for ($numero = 1; $numero <= 20; $numero++) {
+  if ($numero % 1 == 0) {
+    echo $numero . "\n";
+  }
+}
+?>
---
+++ ejercicio2.php
@@ -0,0 +1,8 @@
+<?php
+$suma_total = 0;
+for ($numero = 1; $numero <= 100; $numero++) {
+  $suma_total += $numero;
+}
+echo "La suma de los números del 1 al 100 es: " . $suma_total;
+?>
+
